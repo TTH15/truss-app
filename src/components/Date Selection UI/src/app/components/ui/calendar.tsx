e54eb46d330aa51b84de43acd1a015ddaa8670d2
H.Takaya <takaya_hiraishi@outlook.jp>
2026-03-27 17:37:17 +0900

    Add framer-motion and motion packages; update tsconfig exclusions

--- src/components/Date Selection UI/src/app/components/ui/calendar.tsx
+++ src/components/Date Selection UI/src/app/components/ui/calendar.tsx
@@ -69,14 +69,16 @@ function Calendar({
         day_hidden: "invisible",
         ...classNames,
       }}
-      components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("size-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("size-4", className)} {...props} />
-        ),
-      }}
+      components={
+        {
+          IconLeft: ({ className, ...props }) => (
+            <ChevronLeft className={cn("size-4", className)} {...props} />
+          ),
+          IconRight: ({ className, ...props }) => (
+            <ChevronRight className={cn("size-4", className)} {...props} />
+          ),
+        } as any
+      }
       {...props}
     />
   );
